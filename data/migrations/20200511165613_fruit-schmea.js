450d37c9d30e3493cded59e74707206b3afda3e5
Mel Maki <melody.rackham@gmail.com>
2020-05-11 17:02:23 -0600

    Fruit-schema migration complete

--- data/migrations/20200511165613_fruit-schmea.js
+++ data/migrations/20200511165613_fruit-schmea.js
@@ -1,8 +1,17 @@
-
-exports.up = function(knex) {
-  
+// the changes we want to make 
+exports.up = function (knex) {
+    // create a fruits table 
+    // define the schema 
+    return knex.schema.createTable('fruits', tbl => {
+        tbl.increments();
+        tbl.text('name', 128).unique().notNullable();
+        tbl.decimal('avgWeightOz');
+        tbl.boolean('delicious');
+    })
 };
+// undoes the changes 
+exports.down = function (knex) {
+    // drop the fruits table 
+    return knex.schema.dropTableIfExists('fruits');
 
-exports.down = function(knex) {
-  
 };
